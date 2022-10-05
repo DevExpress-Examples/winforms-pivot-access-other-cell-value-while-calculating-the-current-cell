a1c70d0ef5c5c65b68a91e1c8d8353e9e86efb5d
DevExpressExampleBot <fakeEmail>
2022-10-05 18:15:11 +0400

    Source auto update [skip ci]

--- VB/WinAppCustomCellValue/Form1.vb
+++ VB/WinAppCustomCellValue/Form1.vb
@@ -6,11 +6,13 @@ Imports System
 Namespace WinAppCustomCellValue
 	Partial Public Class Form1
 		Inherits XtraForm
-	
+
 		Public Sub New()
 			InitializeComponent()
 			salesPersonTableAdapter1.Fill(nwindDataSet1.SalesPerson)
+
 			Dim fieldPercentOfBeverages As New PivotGridField() With {.FieldName = "Extended Price", .Area = PivotArea.DataArea, .Caption = "% Beverages", .Name = "PercentOfBeverages"}
+
 			fieldPercentOfBeverages.CellFormat.FormatType = DevExpress.Utils.FormatType.Numeric
 			fieldPercentOfBeverages.CellFormat.FormatString = "P"
 			pivotGridControl1.Fields.Add(fieldPercentOfBeverages)
@@ -19,11 +21,12 @@ Namespace WinAppCustomCellValue
 		Private Sub pivotGridControl1_CustomCellValue(ByVal sender As Object, ByVal e As PivotCellValueEventArgs) Handles pivotGridControl1.CustomCellValue
 			' Calculates the 'Percent' field values.
 			If e.DataField.Name = "PercentOfBeverages" Then
-				' Do not display grand total values.
+				' Hides grand total values.
 				If e.RowValueType = PivotGridValueType.GrandTotal Then
 					e.Value = Nothing
 					Return
 				End If
+
 				Dim rowValues = e.GetRowFields().Select(Function(f) If(f Is fieldCategoryName, "Beverages", e.GetFieldValue(f))).ToArray()
 				Dim columnValues = e.GetColumnFields().Select(Function(f) If(f Is fieldCategoryName, "Beverages", e.GetFieldValue(f))).ToArray()
 				Dim beveragesValue As Decimal = Convert.ToDecimal(e.GetCellValue(columnValues, rowValues, e.DataField))
